39619f49a9b2e0b693949cd42647149f06307611
Urbani Ludovico <lu.urbani@outlook.com>
2025-05-18 19:10:04 +0200

    Fixed get_next_image seen list

--- src/loop.c
+++ src/loop.c
@@ -4,18 +4,20 @@
 
 FilesNode* get_next_image(Files* files);
 
+
 gboolean update_image(const gpointer user_data) {
     const LoopData* data = user_data;
 
     const FilesNode* next_node = get_next_image(data->files);
 
-    GtkPicture* next_picture= gtk_stack_get_visible_child(data->stack) == GTK_WIDGET(data->image1) ? data->image2 : data->image1;
+    GtkPicture* next_picture = gtk_stack_get_visible_child(data->stack) == GTK_WIDGET(data->image1) ? data->image2 : data->image1;
     gtk_image_set_from_file(GTK_IMAGE(next_picture), next_node->path);
 
     gtk_stack_set_visible_child(data->stack, GTK_WIDGET(next_picture));
 
     return TRUE;
 }
+
 
 FilesNode* get_next_image(Files* files) {
     if (files->count < 2) {
@@ -28,11 +30,17 @@ FilesNode* get_next_image(Files* files) {
         files->files = files->files->next;
     }
 
+    // Extract node
     FilesNode* node = files->files->next;
-    files->files->next = files->files->next->next;
+    files->files->next = node->next;
 
+    // Seen list
     node->next = nullptr;
-    files->seen_last->next = node;
+    if (files->seen_last == nullptr) {
+        files->seen = node;
+    } else {
+        files->seen_last->next = node;
+    }
     files->seen_last = node;
     files->seen_count++;
     while (files->seen_count > (files->count >= 4 ? files->count / 4 : 1)) {
@@ -41,9 +49,9 @@ FilesNode* get_next_image(Files* files) {
 
         tmp->next = files->files->next;
         files->files->next = tmp;
-        files->files = tmp;
+        files->files = tmp->next;
+        files->seen_count--;
     }
-    files->files = files->files->next;
 
     return node;
 }
