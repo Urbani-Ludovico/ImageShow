abe15fc46b78ec3a03f150303f2a71b14e9b585c
Ludovico Urbani <lu.urbani@outlook.com>
2025-05-19 13:57:37 +0200

    Quit action

--- src/window.c
+++ src/window.c
@@ -22,37 +22,63 @@ int create_window(GtkApplication* app) {
     gtk_window_set_title(window_data.window, "Random Image Show");
     gtk_window_set_default_size(window_data.window, 800, 600);
 
+    //
+    // Key press
+    //
     GtkEventController* key_controller = gtk_event_controller_key_new();
     g_signal_connect(key_controller, "key-pressed", G_CALLBACK(on_escape_pressed), NULL);
     gtk_widget_add_controller(GTK_WIDGET(window_data.window), key_controller);
 
+    //
+    // Stack
+    //
     window_data.stack = GTK_STACK(gtk_stack_new());
     gtk_stack_set_transition_type(window_data.stack, GTK_STACK_TRANSITION_TYPE_CROSSFADE);
     gtk_stack_set_transition_duration(window_data.stack, refresh_interval >= 3000 ? 1000 : refresh_interval / 3);
     gtk_window_set_child(window_data.window, GTK_WIDGET(window_data.stack));
 
+    //
     // Pages
+    //
     create_window_page(&window_data.overlay1, &window_data.image1, &window_data.label1);
     create_window_page(&window_data.overlay2, &window_data.image2, &window_data.label2);
 
+    //
     // Menu
+    //
     window_data.menu_bar = g_menu_new();
+
     window_data.file_menu = g_menu_new();
+    g_menu_append(window_data.file_menu, "Quit", "app.quit");
+
     window_data.presentation_menu = g_menu_new();
 
     g_menu_append_submenu(window_data.menu_bar, "File", G_MENU_MODEL(window_data.file_menu));
     g_menu_append_submenu(window_data.menu_bar, "Presentation", G_MENU_MODEL(window_data.presentation_menu));
 
+    // Create the popover menu
     window_data.menu_button = GTK_MENU_BUTTON(gtk_menu_button_new());
     window_data.menu_popover = GTK_POPOVER_MENU(gtk_popover_menu_new_from_model(G_MENU_MODEL(window_data.menu_bar)));
     gtk_menu_button_set_popover(window_data.menu_button, GTK_WIDGET(window_data.menu_popover));
     gtk_menu_button_set_icon_name(window_data.menu_button, "open-menu-symbolic");
 
+    // Create a header bar and add the menu button
     window_data.header_bar = GTK_HEADER_BAR(gtk_header_bar_new());
     gtk_header_bar_pack_end(window_data.header_bar, GTK_WIDGET(window_data.menu_button));
     gtk_window_set_titlebar(window_data.window, GTK_WIDGET(window_data.header_bar));
 
+    // Create actions
+    GSimpleAction *quit_action = g_simple_action_new("quit", nullptr);
+
+    // Connect action signals
+    g_signal_connect_swapped(quit_action, "activate", G_CALLBACK(g_application_quit), app);
+
+    // Add actions to window and application
+    g_action_map_add_action(G_ACTION_MAP(app), G_ACTION(quit_action));
+
+    //
     // Css
+    //
     char* css = g_strdup_printf("label.image-title { font-size: %dpt; text-shadow: 0px 0px %dpx black; }", label_size, label_size > 10 ? 4 : 1);
     GtkCssProvider* provider = gtk_css_provider_new();
     gtk_css_provider_load_from_string(provider, css);
