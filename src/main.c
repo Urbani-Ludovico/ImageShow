3c99836ceb9a8667ec2d0a6b588777039d57669c
Ludovico Urbani <lu.urbani@outlook.com>
2025-05-22 18:16:15 +0200

    Application non unique

--- src/main.c
+++ src/main.c
@@ -20,6 +20,8 @@ extern Files files;
 int main(const int argc, char** argv) {
     app = gtk_application_new("com.example.imageshow", G_APPLICATION_DEFAULT_FLAGS);
 
+    g_application_set_flags(G_APPLICATION(app), G_APPLICATION_NON_UNIQUE);
+
     const GOptionEntry entries[] = {{"source-path", 'p', 0, G_OPTION_ARG_FILENAME, &source_path, "Source folder path", "FOLDER"}, {"refresh-interval", 'i', 0, G_OPTION_ARG_INT, &refresh_interval, "Refresh rate in milliseconds (>= 100)", "RATE"}, {"title", 't', 0, G_OPTION_ARG_STRING, &add_title, "Add title label: filename, lastdir", "TYPE"}, {"label-size", 's', 0, G_OPTION_ARG_INT, &label_size, "Label size in pt (>= 2)", "24"}, {nullptr},};
     g_application_add_main_option_entries(G_APPLICATION(app), entries);
 
